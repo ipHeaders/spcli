e2f50c2b5eba002a25030f4075610b9c85c62e7a
lmas-chwy <lmas@chewy.com>
2022-08-01 21:34:48 -0400

    updating setup tool

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
-from setuptools import setup
+import pathlib
+
+from setuptools import find_packages, setup
+
+here = pathlib.Path(__file__).parent.resolve()
 
 setup(
     name="sp",
     version='0.1',
     py_modules=['sp'],
+    setup_requires=["setuptools_scm"],
     install_requires=[
         "pyedgeconnect",
-        "PyYAML==6.0"
+        "PyYAML==6.0",
+        "tabulate==0.8.10"
     ],
+    packages=find_packages(),
+    package_dir={"lib": "lib"},
+    zip_safe=False,
     entry_points='''
         [console_scripts]
         sp=sp:main
