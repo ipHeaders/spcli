66842d75c29d1c85bc2a7d7646ccf63ce99db726
Lazaro Mas <mas.lazaro@yahoo.com>
2022-08-05 16:17:01 -0400

    commit

--- setup.py
+++ setup.py
@@ -5,17 +5,14 @@ from setuptools import find_packages,setup
 
 here = pathlib.Path(__file__).parent.resolve()
 
+install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').splitlines()
+
 setup(
-    name="sp",
+    name="pyspcli",
     version='0.0.7',
     py_modules=['sp'],
     setup_requires=["setuptools"],
-    install_requires=[
-        "pyedgeconnect",
-        "PyYAML==6.0",
-        "tabulate==0.8.10",
-        "importlib_metadata",
-    ],
+    install_requires=install_requires
     packages=find_packages(where="spcli"),
     package_dir={"": "spcli"},
     include_package_data=True,
