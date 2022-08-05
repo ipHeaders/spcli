5f476b46527137badb8bfb7d4411ac247a194bd7
Lazaro Mas <mas.lazaro@yahoo.com>
2022-08-05 18:07:01 -0400

    updating version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = (here / 'requirements.txt').read_text(encoding='utf-8').split
 
 setup(
     name="pyspcli",
-    version='0.1.0',
+    version='0.1.1',
     py_modules=['sp'],
     setup_requires=["setuptools"],
     install_requires=install_requires,
